726fa41579509cb7c2d71e076feb6cf593714f6b
Artur de Sousa Rocha <adsr@poczta.onet.pl>
2012-04-24 20:21:02 +0200

    More tests for namespace handling

--- test/test_from_node_namespaces.rb
+++ test/test_from_node_namespaces.rb
@@ -20,4 +20,30 @@ class TestFromNodeNamespaces < Test::Unit::TestCase
     a = AttrWithXPathAndNamespace.new(doc.root)
     assert_equal('xyz', a.c)
   end
+
+
+  class AttrWithXPathAndNamespaceAtEnd
+    include FromNode
+    xpath_attr :c, "n:b/@c"
+    xpath_namespaces "n" => "bar"
+  end
+
+  def test_attr_with_xpath_and_namespace_at_end
+    doc = xml('<m:a xmlns:m="bar"><m:b c="xyz"/></m:a>')
+    a = AttrWithXPathAndNamespaceAtEnd.new(doc.root)
+    assert_equal('xyz', a.c)
+  end
+
+
+  class AttrChildWithXPathAndNamespace
+    include FromNode
+    xpath_namespaces "n" => "bar"
+    xpath_child :x, "n:x", AttrWithXPathAndNamespace
+  end
+
+  def test_attr_child_with_xpath_and_namespace
+    doc = xml('<m:a xmlns:m="bar"><m:x><m:b c="xyz"/></m:x></m:a>')
+    a = AttrChildWithXPathAndNamespace.new(doc.root)
+    assert_equal('xyz', a.x.c)
+  end
 end
